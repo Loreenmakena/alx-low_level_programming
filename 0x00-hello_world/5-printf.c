d4f16cc612664a0ed78a1d0674ba4ebdcc3c31ee
loreenmakena <loreen.mate@gmail.com>
2023-03-18 13:40:08 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+/**
+ * main - Proper grammmer
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+pritf("with proper grammar, but the outcome is a piece of art,\n")
+return (0);
+}
