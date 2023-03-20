3affe279e79dc331beec462e7cb2d1c7f1414005
loreenmakena <loreen.mate@gmail.com>
2023-03-20 12:35:48 -0700

    6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,6 +1,5 @@
 #include<stdio.h>
 /**
- *
  * main - Prints numers from 0 to 9.
  *
  * Return: Always 0 (Success)
@@ -11,6 +10,6 @@ for (i = 48; i < 58; i++)
 {
 putchar(i);
 }
-putchhar('\n');
+putchar('\n');
 return (0);
 }
