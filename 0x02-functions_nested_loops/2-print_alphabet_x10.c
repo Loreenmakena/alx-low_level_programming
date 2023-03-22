7fb9cbe8ed205e52cbc979fe112260476e7de152
loreenmakena <loreen.mate@gmail.com>
2023-03-22 13:18:02 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,8 @@
 #include "main.h"
 /**
- * print_alphabet_x10 or make alphabet x10times
- * Return: void
+ * print_alphabet_x10 - Entry point
+ * Description: 'the program's description'
+ * Return: Always 0 (Success)
  */
 void print_alphabet_x10(void)
 {
