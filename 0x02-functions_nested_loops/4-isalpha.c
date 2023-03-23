4c8ea177c94ac56ed7acf215dc30eb3996811fac
loreenmakena <loreen.mate@gmail.com>
2023-03-23 12:52:34 -0700

    4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -4,7 +4,7 @@
  * @c: The character to be checked
  * Return: 1 for alphabetic character of 0 for anything else
  */
-int _isalpha(int c)
+int _isisalpha(int c)
 {
 if ((c >= 65 && c <=97) || (c >= 97 && c <= 122))
 {
